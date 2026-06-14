2e9a9f163e44fb35f0df85b8335b72850eba090d
arcticfox <hellocpp2x@gmail.com>
2026-06-14 21:41:35 +0800

    refactor(xlog): drop file:line:func column from console output
    The console log path (logcat on Android, printf on iOS/macOS) formatted its
    own prefix independently of log_formater, so it still printed an empty
    [:0, ] / [file, func, line] column for FFI-originated logs that carry no
    __FILE__/__LINE__. Drop that column to match the file output. Android keeps
    only the message (logcat already shows level/tag/pid); unix keeps level+tag.
    OHOS already suppresses the column when empty, so it is unchanged.

--- mars/xlog/jni/ConsoleLog.cc
+++ mars/xlog/jni/ConsoleLog.cc
@@ -20,7 +20,6 @@
 #include <string.h>
 
 #include "mars/comm/autobuffer.h"
-#include "mars/comm/xlogger/loginfo_extract.h"
 #include "mars/comm/xlogger/xloggerbase.h"
 
 namespace mars {
@@ -28,22 +27,10 @@ namespace xlog {
 
 // 这里不能加日志，会导致循环调用
 void ConsoleLog(const XLoggerInfo* _info, const char* _log) {
-    char result_log[16 * 1024] = {0};
     if (_info) {
-        const char* filename = ExtractFileName(_info->filename);
-        const char* strFuncName = NULL == _info->func_name ? "" : _info->func_name;
-
-        snprintf(result_log,
-                 sizeof(result_log),
-                 "[%s:%d, %s]:%s",
-                 filename,
-                 _info->line,
-                 strFuncName,
-                 _log ? _log : "NULL==log!!!");
-        __android_log_write(_info->level + 2, _info->tag ? _info->tag : "", (const char*)result_log);
+        __android_log_write(_info->level + 2, _info->tag ? _info->tag : "", _log ? _log : "NULL==log!!!");
     } else {
-        snprintf(result_log, sizeof(result_log), "%s", _log ? _log : "NULL==log!!!");
-        __android_log_write(ANDROID_LOG_WARN, "", (const char*)result_log);
+        __android_log_write(ANDROID_LOG_WARN, "", _log ? _log : "NULL==log!!!");
     }
 }
 
--- mars/xlog/unix/ConsoleLog.cc
+++ mars/xlog/unix/ConsoleLog.cc
@@ -2,7 +2,6 @@
 // Created by gjt on 10/17/24.
 //
 
-#include "mars/comm/xlogger/loginfo_extract.h"
 #include "mars/comm/xlogger/xlogger.h"
 
 namespace mars {
@@ -18,18 +17,12 @@ void ConsoleLog(const XLoggerInfo* _info, const char* _log) {
         "E",  // error
         "F"   // fatal
     };
-    char strFuncName[128] = {0};
-    ExtractFunctionName(_info->func_name, strFuncName, sizeof(strFuncName));
-    const char* file_name = ExtractFileName(_info->filename);
     char log[16 * 1024] = {0};
     snprintf(log,
              sizeof(log),
-             "[%s][%s][%s, %s, %d][%s\n",
+             "[%s][%s] %s\n",
              levelStrings[_info->level],
              NULL == _info->tag ? "" : _info->tag,
-             file_name,
-             strFuncName,
-             _info->line,
              _log);
     printf("%s", log);
 }

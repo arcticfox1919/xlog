0176c9abf5876f8c77f5f7c5db7d9ceefb5e3e37
arcticfox <hellocpp2x@gmail.com>
2026-06-14 22:40:37 +0800

    chore(xlog): bump verinfo to 1.3.4

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "068ba558"
+#define MARS_REVISION "853866c2"
 #define MARS_PATH "flutter_plugin"
 #define MARS_URL "git@github.com:arcticfox1919/xlog.git"
-#define MARS_BUILD_TIME "2026-06-14 21:41:57"
-#define MARS_TAG "1.3.3"
-#define MARS_BUILD_TIMESTAMP 1781444517
+#define MARS_BUILD_TIME "2026-06-14 22:40:22"
+#define MARS_TAG "1.3.4"
+#define MARS_BUILD_TIMESTAMP 1781448022
 
 #endif
